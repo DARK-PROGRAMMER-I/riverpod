2606de067616eebcfc3beaa2c52ce0db22a3a669
NewUser <hammadmalli64@gmail.com>
2022-12-30 11:08:56 +0500

    State provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:river_pod_series/state_provider.dart';
+import 'common_libs.dart';
 
-final nameProvider = Provider((ref) => "Hola Ammar Ge");
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -19,64 +18,8 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       themeMode: ThemeMode.dark,
       darkTheme: ThemeData.dark(),
-      home: Consumer(
-        builder: (_, ref, __){
-          String title = ref.watch(nameProvider);
-          return MyHomePage(title: title);
-        },
-
-      ),
+      home: StateProviderTut(title: "River Pod"),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
---
+++ lib/state_provider.dart
@@ -0,0 +1,49 @@
+import 'common_libs.dart';
+
+final counter = StateProvider((ref) => 0);
+
+class StateProviderTut extends ConsumerStatefulWidget {
+  const StateProviderTut({super.key, required this.title});
+  final String title;
+
+  @override
+  ConsumerState<StateProviderTut> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends ConsumerState<StateProviderTut> {
+
+  @override
+  Widget build(BuildContext context) {
+    int value = ref.watch(counter).toInt();
+    print(value);
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$value',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          ref.read(counter.notifier).update((state) => state+ 1);
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
